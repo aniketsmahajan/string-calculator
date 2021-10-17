94383b0895aebbcf9c8c57e05201b7c52314d99a
aniket mahajan <aniketsmahajan@gmail.com>
2021-10-17 18:09:18 +0530

    sum method added

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -21,5 +21,32 @@ class StringCalculator {
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
+	
+	private static String[] splitNumbers(String numbers, String divider){
+		//System.out.println(divider);
+	    return numbers.split(divider);
+	}
+
+	private static int sum(String[] numbers){
+ 	    int total = 0;
+ 	    String negString = "";
+
+        for(String number : numbers){
+        	if(toInt(number) < 0){
+        		if(negString.equals(""))
+        			negString = number;
+        		else
+        			negString += ("," + number);
+        	}
+        	if(toInt(number) < 1000)
+		    	total += toInt(number);
+		}
+
+		if(!negString.equals("")){
+			throw new IllegalArgumentException("Negatives not allowed: " + negString);
+		}
+
+		return total;
+    }
 
 }
